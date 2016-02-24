6fe08cffc065554a7b3a1fd50fcc262dc3124c21
Vu Dang <jublusion5@gmail.com>
2016-02-23 18:46:40 -0600

    created new entity for core data.

---
+++ Recipe.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe.swift
+//  recipez
+//
+//  Created by Vu Dang on 2/23/16.
+//  Copyright © 2016 Vu Dang. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Recipe: NSManagedObject
+{
+
+// Insert code here to add functionality to your managed object subclass
+
+}
